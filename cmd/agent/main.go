8abe4ee83a8d2c4ea59c1410f31a516799fc3b6f
a-tho <103521121+a-tho@users.noreply.github.com>
2023-06-12 17:40:37 +0400

    Move parseFlags to main.go for agent

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"errors"
+	"flag"
 	"time"
 
 	monitor "github.com/a-tho/monitor/internal"
@@ -27,6 +29,23 @@ func run() error {
 
 	var obs monitor.Observer = telemetry.New(srvAddr, pollInterval, reportStep)
 	obs.Observe()
+
+	return nil
+}
+
+func parseFlags() error {
+	flag.StringVar(&srvAddr, "a", "localhost:8080", "address and port to run server")
+	pi := flag.Int("p", 2, "rate of polling metrics in seconds")
+	ri := flag.Int("r", 10, "rate of reporting metrics in seconds")
+	flag.Parse()
+
+	// Both poll/report intervals must be positive, report interval has to be
+	// greater than and a multiple of poll interval
+	if *pi <= 0 || *ri <= 0 || *ri < *pi || *ri%*pi != 0 {
+		return errors.New("invalid p or r")
+	}
+	pollInterval = time.Duration(*pi) * time.Second
+	reportStep = *ri / *pi
 
 	return nil
 }
