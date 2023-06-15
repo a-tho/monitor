cd852e7813cf87d4d362573c2324e61cbc41cf19
a-tho <103521121+a-tho@users.noreply.github.com>
2023-06-15 13:55:36 +0400

    Split polling and reporting logic

--- internal/telemetry/telemetry.go
+++ internal/telemetry/telemetry.go
@@ -40,60 +40,11 @@ func NewObserver(srvAddr string, pollInterval, reportStep int) *Observer {
 func (o *Observer) Observe() error {
 	pollCount := 0
 	for {
-		// Poll metrics
-		var (
-			t        = pollCount % o.reportStep
-			memStats runtime.MemStats
-		)
-		runtime.ReadMemStats(&memStats)
-		o.polled[t].Gauges["Alloc"] = monitor.Gauge(memStats.Alloc)
-		o.polled[t].Gauges["BuckHashSys"] = monitor.Gauge(memStats.BuckHashSys)
-		o.polled[t].Gauges["Frees"] = monitor.Gauge(memStats.Frees)
-		o.polled[t].Gauges["GCCPUFraction"] = monitor.Gauge(memStats.GCCPUFraction)
-		o.polled[t].Gauges["GCSys"] = monitor.Gauge(memStats.GCSys)
-		o.polled[t].Gauges["HeapAlloc"] = monitor.Gauge(memStats.HeapAlloc)
-		o.polled[t].Gauges["HeapIdle"] = monitor.Gauge(memStats.HeapIdle)
-		o.polled[t].Gauges["HeapInuse"] = monitor.Gauge(memStats.HeapInuse)
-		o.polled[t].Gauges["HeapObjects"] = monitor.Gauge(memStats.HeapObjects)
-		o.polled[t].Gauges["HeapReleased"] = monitor.Gauge(memStats.HeapReleased)
-		o.polled[t].Gauges["HeapSys"] = monitor.Gauge(memStats.HeapSys)
-		o.polled[t].Gauges["LastGC"] = monitor.Gauge(memStats.LastGC)
-		o.polled[t].Gauges["Lookups"] = monitor.Gauge(memStats.Lookups)
-		o.polled[t].Gauges["MCacheInuse"] = monitor.Gauge(memStats.MCacheInuse)
-		o.polled[t].Gauges["MCacheSys"] = monitor.Gauge(memStats.MCacheSys)
-		o.polled[t].Gauges["MSpanInuse"] = monitor.Gauge(memStats.MSpanInuse)
-		o.polled[t].Gauges["MSpanSys"] = monitor.Gauge(memStats.MSpanSys)
-		o.polled[t].Gauges["Mallocs"] = monitor.Gauge(memStats.Mallocs)
-		o.polled[t].Gauges["NextGC"] = monitor.Gauge(memStats.NextGC)
-		o.polled[t].Gauges["NumForcedGC"] = monitor.Gauge(memStats.NumForcedGC)
-		o.polled[t].Gauges["NumGC"] = monitor.Gauge(memStats.NumGC)
-		o.polled[t].Gauges["OtherSys"] = monitor.Gauge(memStats.OtherSys)
-		o.polled[t].Gauges["PauseTotalNs"] = monitor.Gauge(memStats.PauseTotalNs)
-		o.polled[t].Gauges["StackInuse"] = monitor.Gauge(memStats.StackInuse)
-		o.polled[t].Gauges["StackSys"] = monitor.Gauge(memStats.StackSys)
-		o.polled[t].Gauges["Sys"] = monitor.Gauge(memStats.Sys)
-		o.polled[t].Gauges["TotalAlloc"] = monitor.Gauge(memStats.TotalAlloc)
-
-		randomValue := rand.New(rand.NewSource(time.Now().Unix())).Float64()
-		o.polled[t].Gauges["RandomValue"] = monitor.Gauge(randomValue)
+		o.poll(pollCount)
 
 		pollCount++
 		if pollCount%o.reportStep == 0 {
-			// Report to the server
-			for _, instance := range o.polled {
-				// Gauge metrics
-				for key, value := range instance.Gauges {
-					url := fmt.Sprintf("http://%s/%s/%s/%s/%f",
-						o.srvAddr, server.UpdPath, server.GaugePath, key, float64(value))
-					if err := report(url); err != nil {
-						return err
-					}
-				}
-			}
-			// Counter metric
-			url := fmt.Sprintf("http://%s/%s/%s/%s/%d",
-				o.srvAddr, server.UpdPath, server.CounterPath, "PollCount", o.reportStep)
-			if err := report(url); err != nil {
+			if err := o.report(); err != nil {
 				return err
 			}
 		}
@@ -102,7 +53,65 @@ func (o *Observer) Observe() error {
 	}
 }
 
-func report(url string) error {
+func (o *Observer) poll(pollCount int) {
+	var (
+		t        = pollCount % o.reportStep
+		memStats runtime.MemStats
+	)
+	runtime.ReadMemStats(&memStats)
+	o.polled[t].Gauges["Alloc"] = monitor.Gauge(memStats.Alloc)
+	o.polled[t].Gauges["BuckHashSys"] = monitor.Gauge(memStats.BuckHashSys)
+	o.polled[t].Gauges["Frees"] = monitor.Gauge(memStats.Frees)
+	o.polled[t].Gauges["GCCPUFraction"] = monitor.Gauge(memStats.GCCPUFraction)
+	o.polled[t].Gauges["GCSys"] = monitor.Gauge(memStats.GCSys)
+	o.polled[t].Gauges["HeapAlloc"] = monitor.Gauge(memStats.HeapAlloc)
+	o.polled[t].Gauges["HeapIdle"] = monitor.Gauge(memStats.HeapIdle)
+	o.polled[t].Gauges["HeapInuse"] = monitor.Gauge(memStats.HeapInuse)
+	o.polled[t].Gauges["HeapObjects"] = monitor.Gauge(memStats.HeapObjects)
+	o.polled[t].Gauges["HeapReleased"] = monitor.Gauge(memStats.HeapReleased)
+	o.polled[t].Gauges["HeapSys"] = monitor.Gauge(memStats.HeapSys)
+	o.polled[t].Gauges["LastGC"] = monitor.Gauge(memStats.LastGC)
+	o.polled[t].Gauges["Lookups"] = monitor.Gauge(memStats.Lookups)
+	o.polled[t].Gauges["MCacheInuse"] = monitor.Gauge(memStats.MCacheInuse)
+	o.polled[t].Gauges["MCacheSys"] = monitor.Gauge(memStats.MCacheSys)
+	o.polled[t].Gauges["MSpanInuse"] = monitor.Gauge(memStats.MSpanInuse)
+	o.polled[t].Gauges["MSpanSys"] = monitor.Gauge(memStats.MSpanSys)
+	o.polled[t].Gauges["Mallocs"] = monitor.Gauge(memStats.Mallocs)
+	o.polled[t].Gauges["NextGC"] = monitor.Gauge(memStats.NextGC)
+	o.polled[t].Gauges["NumForcedGC"] = monitor.Gauge(memStats.NumForcedGC)
+	o.polled[t].Gauges["NumGC"] = monitor.Gauge(memStats.NumGC)
+	o.polled[t].Gauges["OtherSys"] = monitor.Gauge(memStats.OtherSys)
+	o.polled[t].Gauges["PauseTotalNs"] = monitor.Gauge(memStats.PauseTotalNs)
+	o.polled[t].Gauges["StackInuse"] = monitor.Gauge(memStats.StackInuse)
+	o.polled[t].Gauges["StackSys"] = monitor.Gauge(memStats.StackSys)
+	o.polled[t].Gauges["Sys"] = monitor.Gauge(memStats.Sys)
+	o.polled[t].Gauges["TotalAlloc"] = monitor.Gauge(memStats.TotalAlloc)
+
+	randomValue := rand.New(rand.NewSource(time.Now().Unix())).Float64()
+	o.polled[t].Gauges["RandomValue"] = monitor.Gauge(randomValue)
+}
+
+func (o *Observer) report() error {
+	for _, instance := range o.polled {
+		// Gauge metrics
+		for key, value := range instance.Gauges {
+			url := fmt.Sprintf("http://%s/%s/%s/%s/%f",
+				o.srvAddr, server.UpdPath, server.GaugePath, key, float64(value))
+			if err := send(url); err != nil {
+				return err
+			}
+		}
+	}
+	// Counter metric
+	url := fmt.Sprintf("http://%s/%s/%s/%s/%d",
+		o.srvAddr, server.UpdPath, server.CounterPath, "PollCount", o.reportStep)
+	if err := send(url); err != nil {
+		return err
+	}
+	return nil
+}
+
+func send(url string) error {
 	client := resty.New()
 	_, err := client.R().Post(url)
 	if err != nil {
